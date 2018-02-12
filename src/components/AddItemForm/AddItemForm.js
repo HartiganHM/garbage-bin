94684f5980be5485c8f30df9517c8ab1faf99fc9
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-12 15:44:39 -0700

    Add required-fields wrapper

--- src/components/AddItemForm/AddItemForm.js
+++ src/components/AddItemForm/AddItemForm.js
@@ -14,19 +14,27 @@ class AddItemForm extends Component {
   render() {
     return (
       <div className="AddItemForm">
-        <input className="new-item-name" placeholder="Enter a trash name" />
-        <input
-          className="new-item-name"
-          placeholder="Enter an excuse, hoarder"
-        />
-        <select className="new-item-cleanliness">
-          <option className="cleanliness-placeholder" selected disabled>
-            How filthy is it?
-          </option>
-          <option>Sparkling</option>
-          <option>Dusty</option>
-          <option>Rancid</option>
-        </select>
+        <div className="required-fields">
+          <input
+            className="new-item-name form-item"
+            placeholder="Enter a trash name"
+          />
+          <input
+            className="new-item-reason form-item"
+            placeholder="Enter an excuse, hoarder"
+          />
+          <select className="new-item-cleanliness form-item">
+            <option className="cleanliness-placeholder" selected disabled>
+              How filthy is it?
+            </option>
+            <option>Sparkling</option>
+            <option>Dusty</option>
+            <option>Rancid</option>
+          </select>
+        </div>
+        <span className="submit-button" type="submit">
+          Add it the the pile...
+        </span>
       </div>
     );
   }
