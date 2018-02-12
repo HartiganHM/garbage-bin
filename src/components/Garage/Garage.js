82512e76d9555ea09f51b4511218dc2af21779c6
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-12 16:25:45 -0700

    Add garage door and conditional for class; style door

--- src/components/Garage/Garage.js
+++ src/components/Garage/Garage.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import './Garage.css';
 
-const Garage = ({ items }) => {
+const Garage = (props) => {
+  const { items, isOpen } = props;
+  const doorClass = isOpen ? 'garage-door open' : 'garage-door closed';
+  console.log(doorClass);
+
   return (
     <div className="Garage">
+      <div className={doorClass}></div>
       <ItemList items={items} />
     </div>
   );
