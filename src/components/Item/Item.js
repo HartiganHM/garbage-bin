65f0419c04a65ceaba79526b4305682068813cce
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-12 15:14:30 -0700

    Build cleanlinessDropdown and render on Item component

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,10 +4,27 @@ import './Item.css';
 const Item = props => {
   const { name, reason, cleanliness } = props.data;
 
+  const cleanlinessDropdown = ['Sprakling', 'Dusty', 'Rancid'].map(option => {
+    let isSelected = false;
+
+    if (cleanliness === option) {
+      isSelected = true;
+    }
+
+    return <option value={option} selected={isSelected}>{option}</option>
+  })
+
   return (
     <div className="Item">
       <span className="item-name">{name}</span>
-      <span className="item-reason">{reason}</span>
+      <span className="item-property">
+        <span className="section-header">Reason for hoading</span>
+        <span className="section-text">{reason}</span>
+      </span>
+      <span className="item-property">
+      <span className="section-header">Is it clean?</span>
+      <select className="section-text">{cleanlinessDropdown}</select>
+      </span>
     </div>
   );
 };
