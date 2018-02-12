ae7a2fdd185aaa28ae8d95b41affdd5116b5521d
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-12 14:39:21 -0700

    Fix props destructuring

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Item.css';
 
 const Item = props => {
-  const { name, reason, cleanliness } = props.items;
+  const { name, reason, cleanliness } = props.data;
 
   return (
     <div className="Item">
