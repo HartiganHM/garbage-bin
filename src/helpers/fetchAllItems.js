0b0efff3107aafc71acf3eeeb33ec196acfc31f1
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-12 13:56:58 -0700

    Fix fetch url and error messaging

--- src/helpers/fetchAllItems.js
+++ src/helpers/fetchAllItems.js
@@ -1,11 +1,11 @@
 const fetchAllItems = async () => {
   try {
-    const fetchedItems = await fetch('localhost:3000/api/v1/garage-bin');
+    const fetchedItems = await fetch('http://localhost:3000/api/v1/garage-bin');
     const jsonItems = await fetchedItems.json();
 
     return jsonItems;
   } catch (error) {
-    throw Error(`Error retrieving garage-items: ${error}`);
+    throw new Error(`Error retrieving garage-items: ${error}`);
   }
 };
 
