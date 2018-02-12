55a54b5157739963492ffa78072eff67129f3161
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-12 14:20:14 -0700

    Render initial Garage component

---
+++ src/components/Garage/Garage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Garage.css';
+
+const Garage = ({items}) => {
+  return (
+    <div className="Garage">
+      Me Garage!
+    </div>
+  )
+}
+
+export default Garage;
