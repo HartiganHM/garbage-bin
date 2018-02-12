cda55bc2f192c1342270450a085677bf47742ab9
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-12 13:43:07 -0700

    Build state and componentDidMount; import fetchAllItems helps

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import fetchAllItems from '../../helpers/fetchAllItems';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: []
+    }
+  }
+  async componentDidMount () {
+    const items = await fetchAllItems();
+    this.setState({items})
+  }
+
   render() {
     return (
       <div className="App">
