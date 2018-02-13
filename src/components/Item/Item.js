f044fd0f09c38ae5ddb395c2780663188b6e8364
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-12 17:30:46 -0700

    Adjust item height and fix spacing in compoennt

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,15 +4,21 @@ import './Item.css';
 const Item = props => {
   const { name, reason, cleanliness } = props.data;
 
-  const cleanlinessDropdown = ['Sprakling', 'Dusty', 'Rancid'].map((option, index) => {
-    let isSelected = false;
+  const cleanlinessDropdown = ['Sprakling', 'Dusty', 'Rancid'].map(
+    (option, index) => {
+      let isSelected = false;
 
-    if (cleanliness === option) {
-      isSelected = true;
+      if (cleanliness === option) {
+        isSelected = true;
+      }
+
+      return (
+        <option key={index} selected={isSelected}>
+          {option}
+        </option>
+      );
     }
-
-    return <option key={index} selected={isSelected}>{option}</option>
-  })
+  );
 
   return (
     <div className="Item">
@@ -22,8 +28,8 @@ const Item = props => {
         <span className="section-text">{reason}</span>
       </span>
       <span className="item-property">
-      <span className="section-header">Is it clean?</span>
-      <select className="section-text">{cleanlinessDropdown}</select>
+        <span className="section-header">Is it clean?</span>
+        <select className="section-text">{cleanlinessDropdown}</select>
       </span>
     </div>
   );
