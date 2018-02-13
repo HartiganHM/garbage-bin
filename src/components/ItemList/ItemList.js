9389f88a7edc5838999ef9b2dd5465da60e50ff6
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-12 17:54:45 -0700

    Build itemsByType and redneredItems in ItemList

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -10,9 +10,26 @@ const ItemList = (props) => {
     <Item key={index} data={item} />
   ));
 
+  const itemsByType = items.reduce((itemsByType, item, index) => {
+    if (!itemsByType[item.cleanliness]) {
+      itemsByType[item.cleanliness] = 0;
+    }
+    itemsByType[item.cleanliness] ++;
+
+    return itemsByType;
+  }, {})
+
+  const renderedItems = Object.keys(itemsByType).map(item => <span>{item}:{itemsByType[item]}</span>)
+
   return (
     <div className="ItemList">
       <span className="list-header">Check Out Yer Junk!</span>
+      <div className="garage-count">
+        <span className="total-items">Total Junk: {garageItems.length}</span>
+        <span className="items-type">
+          {renderedItems}
+        </span>
+      </div>
       <div className="item-container">{garageItems}</div>
       <AddItemForm addItem={addItem}/>
     </div>
